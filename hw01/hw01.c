acc78aa77f3705155f9b22efbf97abe069ef6ede
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-10-25 13:43:55 +0900

    :recycle: 100行以内に収めた

--- hw01/hw01.c
+++ hw01/hw01.c
@@ -1,6 +1,5 @@
-#include <assert.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 typedef struct {
   unsigned char red;
@@ -8,14 +7,14 @@ typedef struct {
   unsigned char green;
 } RGB;
 
-
 int main(int argc, char *argv[]) {
   int i;
-  int width, height, bright;
+  int width, height, bright, img_size;
   char firstline[100];
   char secondline[100];
-  RGB rgb[65536];
-  unsigned char gray[65536];
+
+  RGB *rgb;
+  unsigned char *gray;
 
 
   // 画像ファイルの読み込み
@@ -23,109 +22,77 @@ int main(int argc, char *argv[]) {
 
   if (fp == NULL) {
     printf("ファイル %s を開けません.\n", argv[1]);
-    return 0;
-  }
-
-  if (argc < 2) {
+    exit(1);
+  } else if (argc < 2) {
     printf("引数が足りません.\n");
-    return 1;
-  }
-  else if (argc > 2) {
+    exit(1);
+  } else if (argc > 2) {
     printf("指定できる画像ファイルは1つです．\n");
-    return 1;
+    exit(1);
   }
-  assert(argc == 2);
-
 
   // ヘッダ部の読み込み
-  fgets(firstline, sizeof(firstline), fp);    // 形式を読み飛ばす
-  fgets(secondline, sizeof(secondline), fp);  // コメントを呼び飛ばす
-
-  fscanf(fp, "%d", &width);                   // 画素を取得
-  fscanf(fp, "%d", &height);
-  fscanf(fp, "%d", &bright);                  // 最大階調値を取得
-
+  fgets(firstline, sizeof(firstline), fp);                // 形式を読み飛ばす
+  fgets(secondline, sizeof(secondline), fp);              // コメントを呼び飛ばす
+  fscanf(fp, "%d %d\n %d\n", &width, &height, &bright);   // 画素, 最大階調値を取得
 
   // データ部の読み込み
-  fread(rgb, sizeof(RGB), width*height, fp);  // まとめて配列に読み込む
-  fclose(fp);
+  img_size = width * height;
+  rgb = (RGB *)malloc(sizeof(RGB) * img_size);
+  gray = (unsigned char *)malloc(sizeof(unsigned char) * img_size);
+  fread(rgb, sizeof(RGB), img_size, fp);
 
 
-
-  // カラー画像の赤だけを取り出して出力
-  for (i = 0; i < width*height; i++) {
+  // R
+  for (i = 0; i < img_size; i++) {
     gray[i] = rgb[i].red;
   }
 
   fp = fopen("red.ppm", "wb");
-
-  fprintf(fp, "P5\n");
-  fprintf(fp, "# Created by IrfanView\n");
-  fprintf(fp, "%d %d\n", width, height);
-  fprintf(fp, "%d\n", bright);
-  fwrite(gray, sizeof(gray[0]), sizeof(gray) / sizeof(gray[0]), fp);
-  fclose(fp);
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(gray, sizeof(unsigned char), img_size, fp);
 
 
-  // カラー画像の緑だけを取り出して出力
-  for (i = 0; i < width*height; i++) {
+  // G
+  for (i = 0; i < img_size; i++) {
     gray[i] = rgb[i].green;
   }
 
   fp = fopen("green.ppm", "wb");
-
-  fprintf(fp, "P5\n");
-  fprintf(fp, "# Created by IrfanView\n");
-  fprintf(fp, "%d %d\n", width, height);
-  fprintf(fp, "%d\n", bright);
-  fwrite(gray, sizeof(gray[0]), sizeof(gray) / sizeof(gray[0]), fp);
-  fclose(fp);
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(gray, sizeof(unsigned char), img_size, fp);
 
 
-  // カラー画像の青だけを取り出して出力
-  for (i = 0; i < width*height; i++) {
+  // B
+  for (i = 0; i < img_size; i++) {
     gray[i] = rgb[i].blue;
   }
 
   fp = fopen("blue.ppm", "wb");
-
-  fprintf(fp, "P5\n");
-  fprintf(fp, "# Created by IrfanView\n");
-  fprintf(fp, "%d %d\n", width, height);
-  fprintf(fp, "%d\n", bright);
-  fwrite(gray, sizeof(gray[0]), sizeof(gray) / sizeof(gray[0]), fp);
-  fclose(fp);
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(gray, sizeof(unsigned char), img_size, fp);
 
 
-  // RGBの平均を求めて出力
-  for (i = 0; i < width*height; i++) {
+  // RGBの平均
+  for (i = 0; i < img_size; i++) {
     gray[i] = (rgb[i].red + rgb[i].green + rgb[i].blue) / 3;
   }
 
   fp = fopen("avarage.ppm", "wb");
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(gray, sizeof(unsigned char), img_size, fp);
 
-  fprintf(fp, "P5\n");
-  fprintf(fp, "# Created by IrfanView\n");
-  fprintf(fp, "%d %d\n", width, height);
-  fprintf(fp, "%d\n", bright);
-  fwrite(gray, sizeof(gray[0]), sizeof(gray) / sizeof(gray[0]), fp);
-  fclose(fp);
-
-
-  // 輝度Yを求めて出力
-  for (i = 0; i < width*height; i++) {
+  // 輝度Y
+  for (i = 0; i < img_size; i++) {
     gray[i] = rgb[i].red * 0.299 + rgb[i].green * 0.587 + rgb[i].blue * 0.114;
   }
 
   fp = fopen("bright.ppm", "wb");
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(gray, sizeof(unsigned char), img_size, fp);
 
-  fprintf(fp, "P5\n");
-  fprintf(fp, "# Created by IrfanView\n");
-  fprintf(fp, "%d %d\n", width, height);
-  fprintf(fp, "%d\n", bright);
-  fwrite(gray, sizeof(gray[0]), sizeof(gray) / sizeof(gray[0]), fp);
+
   fclose(fp);
 
   return 0;
-
 }
