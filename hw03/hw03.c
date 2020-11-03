6a79913bd9f59f27ac168bd05fb6044c5bcab138
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-11-03 12:16:44 +0900

    :tada: 平滑化

--- hw03/hw03.c
+++ hw03/hw03.c
@@ -10,7 +10,6 @@ typedef struct {
 int main(int argc, char *argv[]) {
   int width, height, bright, img_size;
   char firstline[100], secondline[100];
-
   GRAY gray[256][256];
 
   FILE *fp, *gp;
@@ -40,23 +39,23 @@ int main(int argc, char *argv[]) {
   fread(gray, sizeof(GRAY), img_size, fp);
 
 
-  // 濃度度数分布表を生成 ＆ gnulotでヒストグラムを描画
+  // 濃度度数分布表を生成＆gnuplotでヒストグラムを描画
   int hist[256];
-  int max_frequency;
+  int max_freq;
   int i, x, y;
 
-  memset(hist, 0, sizeof(hist));
+  memset(hist, 0, sizeof(hist));    // 配列を初期化
 
   if ((gp = popen("gnuplot -persist", "w")) == NULL) {
     printf("gnuplotを開けません.");
     exit(1);
   }
+
   fprintf(gp,"set boxwidth 1\n");
   fprintf(gp, "set xrange [%d:%d]\n", 0, 256);
   fprintf(gp, "set yrange [%d:%d]\n", 0, 3500);
   fprintf(gp, "plot '-' with boxes\n");
 
-  printf("\n濃度度数分布表を出力しています...\n\n");
 
   for (x = 0; x < width; x++) {
     for (y = 0; y < height; y++) {
@@ -64,17 +63,67 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  max_frequency = 0;
+  printf("\n%s\n\n", "---- 濃度度数分布表 ----");
+  max_freq = 0;
 
-  for (i = 0; i < 256; i++) {
-    if(hist[i] > max_frequency) {
-      max_frequency = hist[i];
+  for (i = 0; i <= bright; i++) {
+    if(hist[i] > max_freq) {
+      max_freq = hist[i];
     }
-    printf("濃度 %3d ：頻度 %d\n", i, hist[i]);
+    printf("濃度：%3d    頻度：%4d\n", i, hist[i]);
     fprintf(gp, "%d\t%d\n", i, hist[i]);
   }
   fprintf(gp, "e\n");
-  printf("\n頻度の最大値 = %d\n\n", max_frequency);
+  printf("\n頻度の最大値 = %d\n\n", max_freq);
+
+
+  // ヒストグラムを平滑化
+  int target_bright = 64;                         // 目標階調
+  int target_freq = img_size / target_bright;     // 目標頻度
+  int sum_freq = 0;                               // 累積頻度
+  int absolute_value = target_freq - sum_freq;    // 目標頻度と蓄積頻度の差の絶対値
+  int cor_thick[bright];                          // 補正濃度
+
+  memset(cor_thick, 0, sizeof(cor_thick));
+
+  // 濃度変換表作成
+  for (i = 0; i <= bright; i++) {
+    /*
+      「目標頻度と蓄積頻度の差の絶対値」 と
+      「目標頻度と蓄積頻度 と 現在の濃度の頻度 の差の絶対値」 を比較
+    */
+
+    // 後者が大きい場合
+    if (abs(absolute_value) < abs(absolute_value - hist[i])) {
+      sum_freq = 0;                               // 累積頻度を0にする
+      cor_thick[i]++;                             // 補正頻度を1加算
+
+      if (i == 0)                                 // 補正濃度が加算されていない場合, 前の補正濃度の値は0
+        cor_thick[i - 1] = 0;
+
+      if (cor_thick[i] >= target_bright)          // 加算した値が目標階調数以上ならば, (目標階調数 - 1)に補正
+        cor_thick[i] = target_bright - 1;
+
+    // 前者が大きい場合
+    } else {
+      cor_thick[i] = cor_thick[i - 1];            // 現在の濃度変換表の値を補正濃度の値とする
+
+      if (i == 0)                                 // 補正濃度が加算されていない場合, 前の補正濃度の値は0
+        cor_thick[i - 1] = 0;
+    }
+    sum_freq += hist[i];                          // 蓄積頻度に現在の濃度の頻度を加算
+  }
+
+
+  char smooth[img_size];
+
+  for (i = 0; i < img_size; i++) {
+    smooth[i] = cor_thick[gray[x][y].data] * ((bright + 1) / target_bright);
+  }
+
+  fp = fopen("smooth.pgm", "wb");
+  fprintf(fp, "P5\n# Grayscale image\n%d %d\n%d\n", width, height, bright);
+  fwrite(smooth, sizeof(GRAY), img_size, fp);
 
 
   pclose(gp);
